d492a02d61e5722b140568b71100b085ade86eb6
Niklas Dewally <niklas@dewally.com>
2025-06-12 18:09:46 +0100

    updated minizinc data

--- minizinc/01-gather-times.sh
+++ minizinc/01-gather-times.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+ns=$(seq 10 10 200)
 
-parallel --progress --eta --no-notice --joblog joblog --resume --timeout 3600 -j4 minizinc models/{1}.mzn models/{2}.dzn\
+for i in $ns; do
+  echo "n = $i;" > "models/$i.dzn"
+done
+
+parallel --progress --eta --no-notice --joblog joblog --resume --timeout 3600 -j4 minizinc -c --solver chuffed models/{1}.mzn models/{2}.dzn\
   ::: $(find models/ -iname '*.mzn' -exec basename {} .mzn \; | sort) \
   ::: $(find models/ -iname '*.dzn' -exec basename {} .dzn \; | sort)\
   ::: $(seq 1 3)
--- minizinc/02-make-times-dat.sh
+++ minizinc/02-make-times-dat.sh
@@ -1,1 +1,1 @@
- awk 'NR>1 {print $4, $10, $11}' joblog  | sed 's/models\///g' | sed 's/\.mzn//g' | sed 's/\.dzn//g' > times.dat
+ awk 'NR>1 {print $4, $13, $14}' joblog  | sed 's/models\///g' | sed 's/\.mzn//g' | sed 's/\.dzn//g' > times.dat
