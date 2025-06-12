68f17c36290e881d34279301cc824b5c5da92717
Niklas Dewally <niklas@dewally.com>
2025-06-12 18:11:07 +0100

    minizinc graphs

--- minizinc/03-process-and-plot.R
+++ minizinc/03-process-and-plot.R
@@ -9,7 +9,7 @@ df <- read_table("times.dat",col_names=c("time_s","model","n"))
 df_g <- df %>% group_by(`model`,`n`) %>% summarise(mean_time_s=mean(`time_s`), sd=sd(`time_s`), samples=n())
 df_g
 
-write_csv(df_g,"avgtimes.csv")
+write_csv(df_g,"avg_times.csv")
 
 
 df_times_slower <- df_g %>% mutate(`prev` = lag(`mean_time_s`)) %>% summarise(`times_slower`=`mean_time_s`/`prev`)
