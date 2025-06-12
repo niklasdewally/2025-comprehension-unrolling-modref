aadc8e40f74c159adc24914f70f98fe055282815
Niklas Dewally <niklas@dewally.com>
2025-06-12 09:57:03 +0100

    some plots for sr

--- sr-timings/01-run.sh
+++ sr-timings/01-run.sh
@@ -2,4 +2,3 @@
 rm *.param
 parallel --no-notice 'echo letting n be {} > {}.param' ::: $(seq -w 25 25 300)
 parallel --no-notice --eta --progress --joblog log --timeout 3600 -j4 savilerow -O0 {1} {2} ::: *.eprime ::: *.param ::: $(seq 1 3)
-cat log | sort -n | cut -f 4,7,9- > times.dat
