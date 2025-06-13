7287f6b0f8a1d91c750aae4d8f8d5f23b05d5cea
Niklas Dewally <niklas@dewally.com>
2025-06-13 10:05:24 +0100

    sr timings

--- sr-timings/01-run.sh
+++ sr-timings/01-run.sh
@@ -1,4 +1,4 @@
 
 rm *.param
-parallel --no-notice 'echo letting n be {} > {}.param' ::: $(seq -w 25 25 300)
+parallel --no-notice 'echo letting n be {} > {}.param' ::: $(seq -w 10 10 200)
 parallel --no-notice --eta --progress --joblog log --timeout 3600 -j4 savilerow -O0 {1} {2} ::: *.eprime ::: *.param ::: $(seq 1 3)
